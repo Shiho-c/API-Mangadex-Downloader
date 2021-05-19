a9bea79458f2e40e2b8814ec727aaec86d0950a3
Shiho <shihohohohohohoho@gmail.com>
2021-05-19 14:50:19 +0800

    can now load chapter to listbox but still cant refresh chapter listbox

--- downloader.py
+++ downloader.py
@@ -49,28 +49,31 @@ class Window(QMainWindow):
                 self.searched_dict[results[x]["data"]["attributes"]["title"]["en"]][b] = results[x]["data"]["attributes"][b]
         #print(self.searched_dict)
         for titles in self.searched_dict:
-            self.list_widget.addItem(titles)
+            self.title_listbox.addItem(titles)
 
 
     def selection_changed(self, item):
         manga_name = item.text()
         #print("Selected item id: ", self.searched_dict[item.text()]["id"])
         if not manga_name in self.clicked_dict.keys():
+            print("Triggering")
             self.clicked_dict[manga_name] = {}
             self.searched_chaps[manga_name] = {}
             self.searched_chaps[manga_name]["chapters"] = {}
             
             url = links["manga_feed"].format(self.searched_dict[item.text()]["id"])
-            params = {"limit":500, "order[chapter]" : "asc"}
+            params = {"limit":100, "order[chapter]" : "asc", "locales[]" : "en"}
             response = requests.get(url, params=params)
             result = response.json()['results']
+            tmp_list = []
+            for x in range(len(result)):
+               self.searched_chaps[manga_name]["chapters"]["Chapter " + str(result[x]['data']['attributes']['chapter'])] = result[x]['data']['attributes']['data']
+               self.chapter_listbox.addItem("Chapter " + str(result[x]['data']['attributes']['chapter']))
+               #self.searched_chaps[manga_name]["chapters"]['title'] = result[x]['data']['attributes']['title']
+                
+            print(tmp_list)
             print(self.searched_chaps)
-            #for x in range(len(result)):
-               # self.searched_chaps[manga_name]
-            
 
-            
-            
         else:
             print("{} is already in the clicked titles\nClicked titles: {}".format(manga_name, self.clicked_dict))
           
@@ -80,19 +83,22 @@ class Window(QMainWindow):
     def UiComponents(self):
   
         # creating widgets
-        self.list_widget = QListWidget(self)
+        self.title_listbox = QListWidget(self)
+        self.chapter_listbox = QListWidget(self)
         self.search_box = QLineEdit(self)
         search_button = QPushButton("bruh", self)
 
         # setting widgets coordinates
         self.search_box.setGeometry(0, 0, 100, 20)
         search_button.setGeometry(self.search_box.geometry().x() + self.search_box.geometry().width() + 20, 0, 100,20)
-        self.list_widget.setGeometry(0, self.search_box.geometry().x() + self.search_box.geometry().height() + 20, 150, 200)
+        self.title_listbox.setGeometry(0, self.search_box.geometry().x() + self.search_box.geometry().height() + 20, 150, 200)
+        self.chapter_listbox.setGeometry(self.title_listbox.geometry().x()+self.title_listbox.geometry().width() + 20, self.search_box.geometry().x() + self.search_box.geometry().height() + 20, 150, 200)
         
 
         #setting up widgets' functions
         search_button.clicked.connect(self.clicked_search)
-        self.list_widget.itemClicked.connect(self.selection_changed)
+        self.title_listbox.itemClicked.connect(self.selection_changed)
+        self.chapter_listbox.itemClicked.connect(self.selection_changed)
 
 
         # list widget items
@@ -101,22 +107,26 @@ class Window(QMainWindow):
             response = requests.get(random)        
             object = QListWidgetItem(str(response.json()['data']['attributes']['title']))
   
-            list_widget.addItem(object)'''
+            title_listbox.addItem(object)'''
 
 
   
         # scroll bar
-        scroll_bar = QScrollBar(self)
+        titles_scrollbar = QScrollBar(self)
+        chapters_scrollbar = QScrollBar(self)
   
         # setting style sheet to the scroll bar
-        scroll_bar.setStyleSheet("background : lightgreen;")
+        titles_scrollbar.setStyleSheet("background : lightgreen;")
+        chapters_scrollbar.setStyleSheet("background : lightgreen;")
   
         # setting vertical scroll bar to it
-        self.list_widget.setVerticalScrollBar(scroll_bar)
+        self.title_listbox.setVerticalScrollBar(titles_scrollbar)
+        self.chapter_listbox.setVerticalScrollBar(chapters_scrollbar)
   
   
         # getting vertical scroll bar
-        value = self.list_widget.verticalScrollBar()
+        value = self.title_listbox.verticalScrollBar()
+        value2 = self.chapter_listbox.verticalScrollBar()
   
   
   

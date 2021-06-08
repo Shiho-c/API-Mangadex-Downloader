9875781bc9a7af84a73450bf46f3c6d5f598b59a
Hoshiiiii <shihohohohohohoho@gmail.com>
2021-06-08 19:06:35 +0800

    can queue manga but not chapters and buggy

--- api_functions.py
+++ api_functions.py
@@ -1,10 +1,11 @@
 import requests 
+import os, errno
+from threading import Thread
 from api_details import links, find_nani, tags
+from custom_functions import download_image
 
 
-def fetch_base_url(chaps_dict, title, current_chap):
-    manga_id = chaps_dict[title][current_chap]["id"]
-    manga_hash = chaps_dict[title][current_chap]["hash"]
+def fetch_base_url(manga_id, manga_hash):
     response = requests.get(links["get_baseurl"].format(manga_id))
     base_url = response.json()["baseUrl"]
     final_base_url = "{}/data/{}/".format(base_url, manga_hash)
@@ -15,7 +16,6 @@ def fetch_titles(results, searched_dict, listbox):
         searched_dict[results[x]["data"]["attributes"]["title"]["en"]] = {}
         searched_dict[results[x]["data"]["attributes"]["title"]["en"]]["Doujinshi"] = "False"
         searched_dict[results[x]["data"]["attributes"]["title"]["en"]]["id"] = results[x]["data"]["id"]
-        searched_dict[results[x]["data"]["attributes"]["title"]["en"]]["description"] = results[x]["data"]["attributes"]["description"]["en"]
         
         for y in results[x]["data"]["attributes"]["tags"]:
             if tags["Doujinshi"] == y["id"]:
@@ -23,28 +23,57 @@ def fetch_titles(results, searched_dict, listbox):
                 break
         for b in find_nani:
             searched_dict[results[x]["data"]["attributes"]["title"]["en"]][b] = results[x]["data"]["attributes"][b]
-    titles = []
-    for t in searched_dict:
-        titles.append(t)
-    return titles
+    for title in searched_dict:
+        listbox.addItem(title)
 
-def fetch_chaps(manga_name, searched_dict, searched_chaps, searched_chaps_info, listbox):
+
+def fetch_key_hash(manga_name, searched_dict):
     url = links["manga_feed"].format(searched_dict[manga_name]["id"])
     offset = 0
     
     params = {"limit":500, "order[chapter]" : "asc", "translatedLanguage[]" : "en", "offset": offset}
     max_result = 500
+    response = requests.get(url, params=params)
+    result = response.json()['results']
+    #while max_result == 500:
+    params["offset"] = offset
+    response = requests.get(url, params=params)
+    result = response.json()['results']
+    while max_result == 500:
+        for x in range(len(result)):
+            manga_id = result[x]['data']['id']
+            manga_hash = result[x]['data']['attributes']['hash']
+            manga_chapter = "Chapter " + str(result[x]['data']['attributes']['chapter'])
+            chapter_directory = os.path.join(os.getcwd(), manga_name, manga_chapter)
+            os.makedirs(chapter_directory)
+            images = result[x]['data']['attributes']['data']
+            base_url = fetch_base_url(manga_id, manga_hash) + "/"
+
+            download_image(base_url, images, chapter_directory, manga_chapter)
+            
+        max_result = len(result)
+        offset+=500
+    print("Finished downloading Manga!")
+
+
+def fetch_chaps(manga_name, searched_dict, searched_chaps, listbox):
+    url = links["manga_feed"].format(searched_dict[manga_name]["id"])
+    offset = 0
+    
+    params = {"limit":500, "order[chapter]" : "asc", "translatedLanguage[]" : "en", "offset": offset}
+    max_result = 500
+    searched_chaps[manga_name] = {}
+    searched_chaps[manga_name]["Chapters"] = []
     while max_result == 500:
         params["offset"] = offset
         response = requests.get(url, params=params)
         result = response.json()['results']
         for x in range(len(result)):
-            searched_chaps[manga_name]["chapters"]["Chapter " + str(result[x]['data']['attributes']['chapter'])] = result[x]['data']['attributes']['data']
+            #searched_chaps[manga_name]["chapters"]["Chapter " + str(result[x]['data']['attributes']['chapter'])] = result[x]['data']['attributes']['data']
             #create a new key called chapter then  store manga id and hash there 
-            searched_chaps_info[manga_name]["Chapter " + str(result[x]['data']['attributes']['chapter'])] = {}
-            searched_chaps_info[manga_name]["Chapter " + str(result[x]['data']['attributes']['chapter'])]["id"] = result[x]['data']['id']
-            searched_chaps_info[manga_name]["Chapter " + str(result[x]['data']['attributes']['chapter'])]["hash"] = result[x]['data']['attributes']['hash']
-
+            searched_chaps[manga_name]["Chapters"].append("Chapter " + str(result[x]['data']['attributes']['chapter']))
             listbox.addItem("Chapter " + str(result[x]['data']['attributes']['chapter']))
         max_result = len(result)
         offset+=500
+
+
--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from PyQt5.QtCore import *
 from functools import partial
 
 from api_details import links, find_nani, tags
-from api_functions import fetch_base_url, fetch_titles, fetch_chaps
-from custom_functions import convert_to_pdf
+from api_functions import fetch_base_url, fetch_titles, fetch_chaps, fetch_key_hash
 from PIL import Image
 import sys
 import re
@@ -16,55 +15,7 @@ import os, errno
 from threading import Thread
 import queue
 from fpdf import FPDF
-manga_queue = {}
-
-class Manga():
-    def __init__(self):
-        super().__init__()
-        
-
-    def thread_download(self):
-        t = Thread(target=self.download_image)
-        t.start()
-
-
-    def download_image(self):
-        for key in manga_queue:
-            for b in range(len(manga_queue[key]["Chapters"])):
-                current_iterated_chapter = manga_queue[key]["Chapters"][0]
-                print("Currently Downloading: {} {}".format(key, current_iterated_chapter))
-                del manga_queue[key]["Chapters"][0]
-                current_iterated_images = manga_queue[key][current_iterated_chapter]["image_list"]
-                current_iterated_base_url = manga_queue[key][current_iterated_chapter]["base_url"]
-                
-                try:
-                    os.makedirs(key)
-                except OSError as e:
-                    if e.errno != errno.EEXIST:
-                        raise
-                if not os.path.exists(current_iterated_chapter):
-                    current_chapdir = os.path.join(os.getcwd(), key, current_iterated_chapter)
-                    try:
-                        os.makedirs(current_chapdir)
-                    except OSError as e:
-                        if e.errno != errno.EEXIST:
-                            raise
-                    for x in range(len(current_iterated_images)):
-                        with open('{}/{}.{}'.format(current_chapdir, x, current_iterated_images[x][-3:]), 'wb+') as handle:
-                                image_url = current_iterated_base_url + current_iterated_images[x]
-                                response = requests.get(image_url, stream=True)
-
-
-                                if not response.ok:
-                                    pass
-
-                                for block in response.iter_content(1024):
-                                    if not block:
-                                        break
-
-                                    handle.write(block)
-    
-                convert_to_pdf(current_chapdir, current_iterated_chapter)
+manga_queue = queue.Queue()
 
 
 class Window(QWidget):
@@ -73,15 +24,18 @@ class Window(QWidget):
         super().__init__()
   
         self.setWindowTitle("Python ")
+        t = Thread(target=self.manga_queue_worker)
+        t.start()
         window_width, window_height = 794, 511
         window_startingpoint = 0
-        self.setFixedSize(window_width, window_height)
+        self.setGeometry(window_startingpoint, window_startingpoint, window_width, window_height)
         #self.setGeometry(window_startingpoint, window_startingpoint, window_width, window_height)
         self.UiComponents()
   
         #no idea where to put variables
         self.searched_dict = {}
-        self.clicked_dict = {}
+        self.manga_info = {}
+        self.clicked_manga_list = []
         self.tmp_data_dict = {}
         self.searched_chaps = {}
         self.searched_chaps_info = {}
@@ -93,6 +47,11 @@ class Window(QWidget):
         self.center()
         self.show()
         
+        
+    def closeEvent(self, event):
+        pass
+
+
     def set_button_flat(self, button, change_image, icon_dir=None):
         button.setFlat(True)
         button.setStyleSheet("QPushButton:pressed {background-color: transparent;}")
@@ -109,7 +68,8 @@ class Window(QWidget):
         self.main_layout = QGridLayout(self)
 
         
-        download_button = QPushButton("Download", self)
+        download_chapter_button = QPushButton("Download Chapter", self)
+        download_manga_button = QPushButton("Download Manga", self)
         search_button = QPushButton("Search", self)
         manga_header = QLabel("Manga Titles: ",self)
         chapter_header = QLabel("Chapters: ",self )
@@ -118,8 +78,8 @@ class Window(QWidget):
         
         
         self.set_button_flat(search_button, True, "components/search.png")
-        self.set_button_flat(download_button, True, "components/download.png")
-        #widget_list = [self.title_listbox, self.chapter_listbox,self.search_box, search_button]
+        self.set_button_flat(download_chapter_button, True, "components/download.png")
+        self.set_button_flat(download_manga_button, True, "components/download.png")
 
 
 
@@ -129,7 +89,8 @@ class Window(QWidget):
         hlayout.setContentsMargins(0,0,0,0)
         hlayout.addWidget(self.search_box)
         hlayout.addWidget(search_button)
-        hlayout.addWidget(download_button)
+        hlayout.addWidget(download_chapter_button)
+        hlayout.addWidget(download_manga_button)
         # add QHLayout to second row in grid layout
         self.main_layout.addLayout(hlayout, 1, 0, 1, 2)
 
@@ -145,7 +106,8 @@ class Window(QWidget):
         chapter_header.setFont(QFont("Yu Gothic UI Light", 16))
         #setting up widgets' functions / signals
         search_button.clicked.connect(self.clicked_search)
-        download_button.clicked.connect(self.clicked_download)
+        download_chapter_button.clicked.connect(self.clicked_download_chapter)
+        download_manga_button.clicked.connect(self.clicked_download_manga)
         self.title_listbox.itemClicked.connect(self.title_box_selectionChanged)
         self.chapter_listbox.itemClicked.connect(self.chapter_box_box_selectionChanged)
         self.doujin_checkbox.stateChanged.connect(self.doujin_checkboxToggled)
@@ -175,32 +137,34 @@ class Window(QWidget):
         frameGm.moveCenter(centerPoint)
         self.move(frameGm.topLeft())
         
-    
-    def clicked_download(self):
-        
-        for s_chapter in self.chapter_listbox.selectedItems():
-            current_chapter = s_chapter.text()
-            manga_queue[self.last_selected_title]["Chapters"].append(current_chapter)
-            manga_queue[self.last_selected_title][current_chapter] = {}
-            manga_queue[self.last_selected_title][current_chapter]["base_url"] = fetch_base_url(self.searched_chaps_info, self.last_selected_title, current_chapter)
-            manga_queue[self.last_selected_title][current_chapter]["image_list"] = self.searched_chaps[self.last_selected_title]["chapters"][current_chapter]
-        #print()
-        m = Manga()
-        m.thread_download()
-            #base_url = fetch_base_url(self.searched_chaps_info, self.last_selected_title, current_chapter)
 
-            #image_list = self.searched_chaps[self.last_selected_title]["chapters"][current_chapter]
-            #m = Manga(image_list, self.last_selected_title, current_chapter, base_url)
-            #m.thread_download()
-            
-            
+    def manga_queue_worker(self):
+        while True:
+            manga_name = manga_queue.get()
+            print(f'Working on {manga_name}')
+            os.makedirs(os.path.join(os.getcwd(), manga_name))
+            fetch_key_hash(manga_name, self.searched_dict)
+            print(f'Finished {manga_name}')
+            manga_queue.task_done()
 
+
+    def clicked_download_manga(self):
+        manga_queue.put(self.last_selected_title)
+        print(self.last_selected_title, " has been added to the queue list!")
+        #t = Thread(target=self.add_queue_info)
+        #t.start()
+
+
+    def clicked_download_chapter(self):
+        pass
+                    
 
     def clicked_search(self):
         #print(self.hidden_title_rows)
         self.hidden_title_rows.clear()
         self.title_listbox.clear()
         self.searched_dict.clear()
+        self.manga_info.clear()
         manga_title = self.search_box.text()
         if self.doujin_checkbox.isChecked():
             params = {"limit":100, "title":manga_title}
@@ -208,11 +172,8 @@ class Window(QWidget):
             params = {"limit":100, "title":manga_title, "excludedTags[]" : tags["Doujinshi"]}
         response = requests.get(links["search"], params=params)
         results = response.json()["results"]
-        titles = fetch_titles(results, self.searched_dict, self.title_listbox)
-        for title in titles:
-            manga_queue[title] = {}
-            manga_queue[title]["Chapters"] = []
-            self.title_listbox.addItem(title)
+        fetch_titles(results, self.searched_dict, self.title_listbox)
+            
 
 
     def doujin_checkboxToggled(self):
@@ -250,17 +211,16 @@ class Window(QWidget):
             return
         manga_name = item.text()
         self.last_selected_title = manga_name
-        if not manga_name in self.clicked_dict.keys():
-            self.clicked_dict[manga_name], self.searched_chaps[manga_name],self.searched_chaps[manga_name]["chapters"]  = {}, {}, {}
-            self.searched_chaps_info[manga_name] = {}
+        if not manga_name in self.clicked_manga_list:
+            self.clicked_manga_list.append(manga_name)
             self.chapter_listbox.clear()
-            t = Thread(target=fetch_chaps, args = (manga_name, self.searched_dict, self.searched_chaps, self.searched_chaps_info, self.chapter_listbox))
+            t = Thread(target=fetch_chaps, args = (manga_name, self.searched_dict, self.searched_chaps, self.chapter_listbox))
             t.start()
 
                 
         else:
             self.chapter_listbox.clear()
-            for chapters in self.searched_chaps[manga_name]["chapters"]:
+            for chapters in self.searched_chaps[manga_name]["Chapters"]:
                 self.chapter_listbox.addItem(chapters)
             #print("{} is already in the clicked titles\nClicked titles: {}".format(manga_name, self.clicked_dict))
           
